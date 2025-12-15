e147f0704ec79b32b6227be75c10a5b1fd29af19
topalemirfaruk <topal.emirfaruk@ogr.ahievran.edu.tr>
2025-12-15 15:14:56 +0300

    fix: Resolve auto-updater issues and build errors

--- electron/preload.js
+++ electron/preload.js
@@ -1,4 +1,4 @@
-import { contextBridge, ipcRenderer } from 'electron';
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
     onYouTubeCode: (callback) => ipcRenderer.on('youtube-code', (_event, code) => callback(code)),
